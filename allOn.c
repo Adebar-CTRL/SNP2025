f52f3308638728e99571a3a8d917028102a0ce12
Adebar-CTRL <83907329+Adebar-CTRL@users.noreply.github.com>
2025-03-18 17:35:04 +0100

    fully working 3 buttons for brigness minutes and hours

--- allOn.c
+++ allOn.c
@@ -2,6 +2,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+volatile uint8_t seconds = 0;
 volatile uint8_t minutes = 0;
 volatile uint8_t hours = 0;
 int currentBrightnessLevel = 1;
@@ -45,17 +46,27 @@ void setupPorts(void) {
 
 }
 //setup von Knöpfen
-void INT0_Init(void) {
+void INT_Init(void) {
     // Konfiguriere PD2 (INT0) als Eingang mit aktiviertem Pull-Up
     DDRD &= ~(1 << PD2);
     PORTD |= (1 << PD2);
-
     // Konfiguriere INT0: Auslösung an fallender Flanke
     EICRA |= (1 << ISC01);  // ISC01 = 1, ISC00 = 0 => fallende Flanke
     EICRA &= ~(1 << ISC00);
-
     // Aktiviere den externen Interrupt INT0
     EIMSK |= (1 << INT0);
+
+	// === PD1 als normaler Button-Eingang mit Pull-Up (kein Interrupt) === druch: if (!(PIND & (1 << PD1))) {} prüfen
+    DDRD &= ~(1 << PD1);
+    PORTD |= (1 << PD1);
+	
+	  // === PB0 (Pin-Change-Interrupt) als Eingang mit Pull-Up ===
+    DDRB &= ~(1 << PB0);
+    PORTB |= (1 << PB0);
+
+    // Aktivieren von Pin-Change-Interrupts für PB0 (PCINT0)
+    PCICR |= (1 << PCIE0);  // Aktiviere Pin-Change-Interrupt für PCINT[7:0] (Port B)
+    PCMSK0 |= (1 << PCINT0); // PB0 (PCINT0) als Pin-Change-Interrupt aktivieren
 }
 
 
@@ -70,39 +81,69 @@ void displayTime(void) {
 }
 
 void updateClock(void) {
-    minutes++;
-    if (minutes >= 60) {
-        minutes = 0;
-        hours++;
-        if (hours >= 24) {
-            hours = 0;
-        }
-    }
+	seconds++;
+	if(seconds >= 60) {
+		seconds = 0;
+		minutes++;
+	    if (minutes >= 60) {
+	        minutes = 0;
+	        hours++;
+	        if (hours >= 24) {
+	            hours = 0;
+	        }
+	    }
+	}
     displayTime();
 }
 
 //Interrupt der jede Sekunde durch den Quarz ausgelöst wird
 ISR(TIMER2_OVF_vect) {
-    updateClock();  // Erhöhe Minuten jede Sekunde
-}
-
-//Interrupt bei Button 2
-ISR(INT0_vect) {
 	const uint8_t brightness_levels[] = {254, 248, 156, 0};
-    _delay_ms(5); // Debounce
-    if (!(PIND & (1 << PD2))) { // Confirm button is still pressed
+    updateClock();  // Erhöhe Minuten jede Sekunde
+	if (!(PIND & (1 << PD1))) {
 		currentBrightnessLevel = (currentBrightnessLevel + 1) % 4;
     	OCR1A = brightness_levels[currentBrightnessLevel]; // For minutes cathodes (PB1)
     	OCR1B = brightness_levels[currentBrightnessLevel]; // For hours cathodes (PB2)
 	}
 }
 
+//Interrupt bei Button 2
+ISR(INT0_vect) {
+	_delay_ms(5); // Debounce
+    if (!(PIND & (1 << PD2))) {  // Prüfen, ob PB0 gedrückt wurde
+        if (minutes == 59) minutes = 0;
+		else {
+			minutes++;
+			displayTime();
+		}
+    }
+	/*
+	const uint8_t brightness_levels[] = {254, 248, 156, 0};
+    _delay_ms(5); // Debounce
+    if (!(PIND & (1 << PD2))) { // Confirm button is still pressed
+		currentBrightnessLevel = (currentBrightnessLevel + 1) % 4;
+    	OCR1A = brightness_levels[currentBrightnessLevel]; // For minutes cathodes (PB1)
+    	OCR1B = brightness_levels[currentBrightnessLevel]; // For hours cathodes (PB2)
+	}*/
+}
+
+ISR(PCINT0_vect) {
+    _delay_ms(5); // Debounce
+    if (!(PINB & (1 << PB0))) {  // Prüfen, ob PB0 gedrückt wurde
+        if (hours == 11) hours = 0;
+		else {
+			hours++;
+			displayTime();
+		}
+    }
+}
 int main(void)
 {
 	int pd_counter = 1;
 	setupPorts();
-	INT0_Init();
+	INT_Init();
 	PWM_Init(); //initialisiere PWM ausgänge für helligkeitsregulierung
+	setupTimer2();
     sei();  // Enable global interrupts
     // Unendliche Schleife
     while (1)
